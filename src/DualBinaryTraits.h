44db56d09dbb68d60dc006d8eb976257568d7293
mimasu <takeknock@gmail.com>
2016-02-01 08:18:47 +0900

    add DualBinaryTraits and functors.

--- src/DualBinaryTraits.h
+++ src/DualBinaryTraits.h
@@ -4,7 +4,7 @@ namespace strcp {
     template <typename L, typename R, typename F>
     class DualBinary {
     public:
-        typedef F<typename L::result_type, typename R::_type> functor_type;
+        typedef F<typename L::result_type, typename R::result_type> functor_type;
         typedef typename functor_type::result_type result_type;
 
         template <typename X>
@@ -16,10 +16,53 @@ namespace strcp {
         typename R::const_closure_type _r;
     };
 
+    // traits
     template <typename L, typename R, template <typename, typename> class F>
     struct DualBinaryTraits {
+        typedef DualBinary<L, R, F> type;
+        static type apply(const L& l, const R& r)
+        {
+            return type(l, r);
+        }
+    };
 
+    // functors
+    template <typename L, typename R>
+    struct DualPlus {
+        typedef F<typename L::result_type, typename R::result_type> result_type;
+        static result_type apply(const L& l, const R& r)
+        {
+            return Dual(x._value + y._value, x._derivative + y._derivative);
+        }
+    };
+    template <typename L, typename R>
+    struct DualMinus {
+        typedef F<typename L::result_type, typename R::result_type> result_type;
+        static result_type apply(const L& l, const R& r)
+        {
+            return Dual(x._value - y._value, x._derivative - y._derivative);
+        }
+    };
 
+    template <typename L, typename R>
+    struct DualMult {
+        typedef F<typename L::result_type, typename R::result_type> result_type;
+        static result_type apply(const L& l, const R& r)
+        {
+            return Dual(x._value * y._value, 
+                x._value * y._derivative + x._derivative * y._value);
+        }
+    };
+
+    template <typename L, typename R>
+    struct DualDiv {
+        typedef F<typename L::result_type, typename R::result_type> result_type;
+        static result_type apply(const L& l, const R& r)
+        {
+            return Dual<result_type>(x._value / y._value,
+                (y._value * x._derivative - x._value * y._derivative)
+                    / (y._value * y._value));
+        }
     };
 
 } // namespace strcp
